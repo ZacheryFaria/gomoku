d8065677948d19488e8a85e6c33c83b66c3279ac
Alexander Carlson <bombblob@gmail.com>
2020-03-07 14:36:10 -0800

    Added timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Board from "./components/Board";
+import Timer from "./components/Timer";
 import "./App.css";
 
 function App() {
   document.title = "gomoku";
   return (
     <div className="App">
+      <Timer />
       <Board />
     </div>
   );
---
+++ src/components/Timer.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const Timer: React.FC<{}> = () => {
+  const [seconds, setSeconds] = useState([0,0]);
+
+  const [player, setPlayer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(([a,b]) => {
+        if (player == 0) {
+          return ([a+1, b]);
+        } else {
+          return ([a, b+1]);
+        }
+      })
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // TODO: add css
+  return (
+    <div>
+      {seconds[0] + " - " + seconds[1]}
+    </div>
+  );
+};
+
+export default Timer
